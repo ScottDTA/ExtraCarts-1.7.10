b4930c874b2411168d22fa98ea70e7e0a3c898ae
SkySom <skylar.som@gmail.com>
2014-10-20 18:11:03 -0400

    Missed a file... Again

--- src/main/java/com/dta/extracarts/mods/ironchest/IronChestModule.java
+++ src/main/java/com/dta/extracarts/mods/ironchest/IronChestModule.java
@@ -2,10 +2,12 @@ package com.dta.extracarts.mods.ironchest;
 
 import com.dta.extracarts.ModInfo;
 import com.dta.extracarts.Module;
+import com.dta.extracarts.mods.ironchest.events.ECEventHandler;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.MinecraftForge;
 
 /**
  * Created by Skylar on 10/17/2014.
@@ -18,8 +20,8 @@ public class IronChestModule implements Module {
 
 	@Override
 	public void load(FMLInitializationEvent event) {
-
-	}
+        MinecraftForge.EVENT_BUS.register(new ECEventHandler());
+    }
 
 	@Override
 	public void postInit(FMLPostInitializationEvent event) {
