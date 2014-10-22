652ef5c6404f405eaba3f57a0b71bcc85ee38635
SkySom <skylar.som@gmail.com>
2014-10-22 17:06:35 -0400

    Added Thermal Expansion module
    IT'S A NEW BEGINNING.

---
+++ src/main/java/com/dta/extracarts/mods/thermalexpansion/ThermalExpansionModule.java
@@ -0,0 +1,13 @@
+package com.dta.extracarts.mods.thermalexpansion;
+
+import com.dta.extracarts.Module;
+
+/**
+ * Created by Skylar on 10/22/2014.
+ */
+public class ThermalExpansionModule extends Module{
+    @Override
+    public String getModuleName() {
+        return "Thermal Expansion";
+    }
+}
