0dc9f4b319d90f09cead219edb0bd99c6b8f2672
SkySom <skylar.som@gmail.com>
2015-03-28 01:51:38 -0400

    Release 0.7.5

--- src/main/java/com/dta/extracarts/ModInfo.java
+++ src/main/java/com/dta/extracarts/ModInfo.java
@@ -10,7 +10,7 @@ public class ModInfo {
 	
 	public static final String NAME = "Extra Carts";
     public static final String MODID = "extracarts";
-    public static final String VERSION = "0.7.4";
+    public static final String VERSION = "0.7.5";
 
     private static ArrayList<Module> MODULES_ENABLED = new ArrayList<Module>();
 
