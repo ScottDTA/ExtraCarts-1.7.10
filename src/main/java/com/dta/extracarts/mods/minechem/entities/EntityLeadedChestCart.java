4c8d7b0aa66708bc016d87554a69add3da357ee4
SkySom <skylar.som@gmail.com>
2015-04-05 23:03:36 -0400

    Created LeadedChestCart Icon
    Also added IMinecartStuff to EntityLeadedChest

--- src/main/java/com/dta/extracarts/mods/minechem/entities/EntityLeadedChestCart.java
+++ src/main/java/com/dta/extracarts/mods/minechem/entities/EntityLeadedChestCart.java
@@ -2,11 +2,14 @@ package com.dta.extracarts.mods.minechem.entities;
 
 import com.dta.extracarts.client.OpenableGUI;
 import com.dta.extracarts.entities.EntityExtraCartChestMinecart;
+import com.dta.extracarts.mods.minechem.MinechemModule;
 import com.dta.extracarts.mods.minechem.client.ContainerLeadedChestCart;
 import com.dta.extracarts.mods.minechem.client.GUILeadedChestCart;
+import cpw.mods.fml.common.Optional;
 import cpw.mods.fml.common.registry.GameRegistry;
 import mods.railcraft.api.carts.IMinecart;
 import net.minecraft.block.Block;
+import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
@@ -15,6 +18,7 @@ import net.minecraft.world.World;
 /**
  * Created by Skylar on 3/30/2015.
  */
+@Optional.Interface(iface = "mods.railcraft.api.carts.IMinecart", modid = "RailcraftAPI|carts")
 public class EntityLeadedChestCart extends EntityExtraCartChestMinecart implements OpenableGUI, IMinecart {
 	private Block leadedChest = GameRegistry.findBlock("minechem", "tile.leadChest");
 
@@ -50,5 +54,14 @@ public class EntityLeadedChestCart extends EntityExtraCartChestMinecart implemen
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		return new ContainerLeadedChestCart(player.inventory, this);
+	}
+
+	@Optional.Method(modid = "RailcraftAPI|carts")
+	public boolean doesCartMatchFilter(ItemStack stack, EntityMinecart cart) {
+		ItemStack CartStack = new ItemStack(MinechemModule.itemLeadedChestCart, 1, 0);
+		if (cart instanceof EntityLeadedChestCart && stack.getItem() == CartStack.getItem() && stack.getItemDamage() == 0) {
+			return true;
+		}
+		return false;
 	}
 }
