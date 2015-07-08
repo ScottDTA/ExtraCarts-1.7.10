c1545461493ca2a0ac494663ad25199908068193
SkySom <skylar.som@gmail.com>
2015-07-08 18:35:47 -0400

    Removed Global ID's
    BAD.

--- src/main/java/com/dta/extracarts/utils/EntityUtils.java
+++ src/main/java/com/dta/extracarts/utils/EntityUtils.java
@@ -7,9 +7,15 @@ import cpw.mods.fml.common.registry.EntityRegistry;
  * Created by Skylar on 4/12/2015.
  */
 public class EntityUtils {
+
+	private static int entityID = 15;
+
 	public static void registerEntity(Class entityClass, String name) {
-		int entityID = EntityRegistry.findGlobalUniqueEntityId();
-		EntityRegistry.registerGlobalEntityID(entityClass, name, entityID);
+		int entityID = getNextAvailableEntityID();
 		EntityRegistry.registerModEntity(entityClass, name, entityID, ExtraCarts.instance, 64, 1, true);
+	}
+
+	private static int getNextAvailableEntityID() {
+		return entityID++;
 	}
 }
